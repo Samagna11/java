6a23a100a577805c65d91596af413933f56dafe2
Samagna11 <70469090+Samagna11@users.noreply.github.com>
2020-10-14 12:23:20 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,14 @@
+package deemo;
+import java.util.*;
+public class Pattern3 {
+public static void main(String[] args) {
+	for(int i=1; i<=5; i++)            //rows
+	{
+		for(int j=1; j<=5; j++)        //columns
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+}
+}
